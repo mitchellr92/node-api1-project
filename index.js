9f41f9259d72346e470f6805e5ae7516966e8ccd
Mitchell Robles <mitchellr92@live.com>
2019-12-06 17:20:35 -0800

    server up and running, get endpoints working

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // implement your API here
+
+const express = require("express");
+let db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+const PORT = 1234;
+const host = "127.0.0.1";
+
+server.get("/", (req, res) => {
+  console.log("ip:", req.connection.remoteAddress);
+
+  res.json({ message: "Welcome to our API" });
+});
+
+server.get("/users", (req, res) => {
+  res.json(db)
+});
+
+server.listen(PORT, host, () => {
+  console.log(`Listening at http://${host}:${PORT}`);
+});
